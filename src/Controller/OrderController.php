1415785aada1239e81443d30bd8da410d16fe248
redwoodcutter <oguzkumcular@gmail.com>
2020-11-09 20:50:58 +0300

    Migrations and Orders Entity, Controller Created

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,83 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Entity\OrderPost;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
+/**
+ * @Route("/order")
+ */
+class OrderController extends AbstractController 
+{   
+    /**
+    * @Route("/{page}", name="order_list", defaults={"page": 5}, requirements={"page"="\d+"})
+    */
+    public function list($page = 1, Request $request)
+    {
+        $limit = $request->get('limit',10);
+        $repository = $this->getDoctrine()->getRepository(OrderPost::class);
+        $items = $repository->findAll();
+
+        return $this->json(
+            [
+                'page' => $page,
+                'limit' => $limit,
+                'data' => array_map(function($item){
+                    return $this->generateUrl('order_by_company_name',['company_name' => $item->getCompanyName()]); // degişcek
+                }, $items)
+            ]
+            
+        );
+    }
+    /**
+     * @Route ("/post/{id}", name="order_by_id", requirements={"id"="\d+"}, methods={"GET"})
+     * @ParamConverter("post", class="App:OrderPost")
+     */
+    public function post($post)
+    {
+        return $this->json($post);
+    }
+    /**
+     * @Route("/post/{company_name}", name="order_by_company_name", methods={"GET"})
+     * @ParamConverter("post", class="App:OrderPost", options={"mapping":{"company_name":"company_name"}})
+     */
+    public function postByCompany($post)
+    {   
+        return $this->json($post);
+    }
+    /**
+     * @Route("/add", name="order_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        /** @var Serizalizer $serializer */
+        $serializer = $this->get('serializer');
+
+        $orderPost = $serializer->deserialize($request->getContent(), OrderPost::class, 'json');
+
+        $em=$this->getDoctrine()->getManager();
+        $em->persist($orderPost);
+        $em->flush();
+
+        return $this->json($orderPost);
+    }
+     /**
+     * @Route("/post/{id}", name="order_delete", methods={"DELETE"})
+     */
+    public function delete(OrderPost $post)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+
+    }
+}
---
+++ src/Entity/OrderPost.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderPostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderPostRepository::class)
+ */
+class OrderPost
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $orderCode;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $shippingDate;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $companyId;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $companyName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOrderCode(): ?int
+    {
+        return $this->orderCode;
+    }
+
+    public function setOrderCode(int $orderCode): self
+    {
+        $this->orderCode = $orderCode;
+
+        return $this;
+    }
+
+    public function getProductId(): ?int
+    {
+        return $this->productId;
+    }
+
+    public function setProductId(int $productId): self
+    {
+        $this->productId = $productId;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getShippingDate(): ?\DateTimeInterface
+    {
+        return $this->shippingDate;
+    }
+
+    public function setShippingDate(\DateTimeInterface $shippingDate): self
+    {
+        $this->shippingDate = $shippingDate;
+
+        return $this;
+    }
+
+    public function getCompanyId(): ?int
+    {
+        return $this->companyId;
+    }
+
+    public function setCompanyId(int $companyId): self
+    {
+        $this->companyId = $companyId;
+
+        return $this;
+    }
+
+    public function getCompanyName(): ?string
+    {
+        return $this->companyName;
+    }
+
+    public function setCompanyName(string $companyName): self
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+}
